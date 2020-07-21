654d6add5ae994aa9aa65b3e2c92296be39147d0
Megan Horsfield <meganthadev@gmail.com>
2020-07-21 06:38:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function addingEventListener() {
 }
 const input = document.getElementById('input');
-input.addEventListener('click', function(event) {
+input.addingEventListener('click', function(event) {
   alert('I was clicked!');
 });
